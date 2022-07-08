b6cd239cefabb51e8fc7c1f64314abfff0af48ce
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-07-08 09:46:49 -0300

    Exercicios de Algoritmo

---
+++ 002 - Calculo da expressão/002 - Calculo da expressão/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/*2.Escreva um algoritmo que leia três números inteiros e positivos (A, B, C) e calcule a seguinte expressão:
+ D = R + S/2 onde R = (A + B) ^2  e  S (B + C) ^2 */
+
+int numero01, numero02, numero03;
+float D, R, S;
+
+Console.WriteLine("Digite o primeiro numero");
+numero01 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo numero");
+numero02 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro numero");
+numero03 = int.Parse(Console.ReadLine());
+
+
+R = numero01 + numero02;
+S = numero02 + numero03;
+
+R = (float)Math.Sqrt(2);
+S = (float)Math.Sqrt(2);
+
+D = (R + S) / 2;
+
+Console.WriteLine("O calculo da expressão é: " + D);
