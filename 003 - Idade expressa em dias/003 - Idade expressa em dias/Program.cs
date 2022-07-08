6408f0dbc78dea7b0dbf9f4676d1d235c5c97dca
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-07-08 12:32:17 -0300

    Exercicios de Algoritmo

---
+++ 003 - Idade expressa em dias/003 - Idade expressa em dias/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* 03.Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e mostre-a expressa apenas em dias.*/
+int dia, mes, anoNascimento, anoAtual, idade =0, quatidadeDias;
+
+Console.WriteLine("Informe seu ano de nascimento");
+anoNascimento = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o dia que você nasceu");
+dia = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o mês que você nasceu");
+mes = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o ano atual");
+anoAtual = int.Parse(Console.ReadLine());
+
+
+idade = anoAtual - anoNascimento;
+
+quatidadeDias = (365 * idade) - 365; 
+mes = (30 * mes) - 30;
+quatidadeDias = quatidadeDias + mes + dia;
+
+Console.WriteLine("Quantidade de dias de vida " +quatidadeDias);
+
+
+
+
+
+//  16060 + 180 + 17
+
+
+
