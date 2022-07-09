9dc3990e2539c8d6fdf837723b26100a6500581a
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-07-09 12:58:59 -0300

    Exercicios de Algoritmo

---
+++ 007 - Porcentagem de custo ao consumidor/007 - Porcentagem de custo ao consumidor/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/******************************************************************************************
+* 07) O custo ao consumidor de um carro novo é a soma do custo de fábrica com a percentagem 
+* do distribuidor e dos impostos (aplicados ao custo de fábrica). Supondo que a percentagem 
+* do distribuidor seja de 28% e os impostos de 45%, escrever um algoritmo que leia o custo 
+* de fábrica de um carro e escreva o custo ao consumidor.
+******************************************************************************************/
+
+float imposto = 0, valorFabrica, percentDistribuidor = 0, valorConsumidor ;
+
+Console.WriteLine("Qual o valor de fábrica do automóvel?");
+valorFabrica = float.Parse(Console.ReadLine());
+
+
+percentDistribuidor = (valorFabrica * 28) / 100;
+imposto = (valorFabrica * 45) / 100;
+valorConsumidor = (valorFabrica + percentDistribuidor + imposto);
+
+Console.WriteLine("O automóvel custará para o consumidor o valor de: R$"+valorConsumidor);
+
+
+
