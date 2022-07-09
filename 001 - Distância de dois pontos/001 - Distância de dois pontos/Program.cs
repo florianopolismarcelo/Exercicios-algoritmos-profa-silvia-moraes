e1ded0a16a0ec62dd3301409aadc52c977df46d0
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-07-09 10:57:07 -0300

    Exercicios de Algoritmo

--- 001 - Distância de dois pontos/001 - Distância de dois pontos/Program.cs
+++ 001 - Distância de dois pontos/001 - Distância de dois pontos/Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+/*************************************************************************************
+* 01) Construa um algoritmo que, tendo como dados de entrada dois pontos quaisquer 
+* no plano, P(x1,y1) e P(x2,y2), escreva a distância entre eles. A fórmula que efetua 
+* tal d = √ (X2 - X1)^2 + (Y2 - Y1)^2
+*************************************************************************************/
 
-/*1. Construa um algoritmo que, tendo como dados de entrada dois pontos quaisquer no plano, P(x1,y1) e P(x2,y2), 
-escreva a distância entre eles. A fórmula que efetua tal d = √ (X2 - X1)^2 + (Y2 - Y1)^2 */
 
 float distancia, valorX1 = 0, valorX2 = 0 , valorY1 = 0, valorY2 = 0;
 
