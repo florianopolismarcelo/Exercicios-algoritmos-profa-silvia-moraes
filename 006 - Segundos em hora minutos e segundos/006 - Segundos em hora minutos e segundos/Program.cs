ec89193cb0709a1955f4dc2eee46cb7e334dc400
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-07-09 10:49:42 -0300

    Exercicios de Algoritmo

---
+++ 006 - Segundos em hora minutos e segundos/006 - Segundos em hora minutos e segundos/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int segundos, horas, minutos;
+
+Console.WriteLine("Informe quantos segundos");
+segundos = int.Parse(Console.ReadLine());
+
+horas = segundos / 3600;
+minutos = segundos / 60;
+segundos = segundos % 60;
+
+Console.WriteLine("A quantidade de segundos digitado tem: " +horas+ " horas " +minutos+ " minutos e " +segundos+ " segundos");
+
+
