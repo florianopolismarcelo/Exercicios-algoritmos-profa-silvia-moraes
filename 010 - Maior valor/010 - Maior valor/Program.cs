e044da09e12ebbbe7c1bd1f251fef7743c77a0e0
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-07-11 10:20:07 -0300

    Exercicios de Algoritmo

--- 010 - Maior valor/010 - Maior valor/Program.cs
+++ 010 - Maior valor/010 - Maior valor/Program.cs
@@ -5,27 +5,30 @@ Elaborar um algoritmo que lê 3 valores a,b,c e os escreve. A seguir, encontre o
 valores e o escreva com a mensagem : "É o maior ".
 *******************************************************************************************/
 
-float numero1, numero2, numero3, maiorValor;
+float numero1, numero2, numero3;
 
-Console.WriteLine("Informe o primeiro numero:");
+Console.WriteLine("Informe o valor de A:");
 numero1 = float.Parse(Console.ReadLine());
 
-Console.WriteLine("Informe o segundo numero:");
+Console.WriteLine("Informe o valor de B:");
 numero2 = float.Parse(Console.ReadLine());
 
-Console.WriteLine("Informe o terceiro numero:");
+Console.WriteLine("Informe o valor de C:");
 numero3 = float.Parse(Console.ReadLine());
 
 if (numero1 > numero2 && numero1 > numero3)
 {
-    Console.WriteLine("O primeiro número: " + numero1 + " tem o maior valor");
+    Console.WriteLine("O valor de B é: "+numero2+ " o valor de C é: "+numero3+
+    " e o valor de A possui o maior valor que é: "+numero1);
 }
 else if (numero2 > numero1 && numero2 > numero3)
 {
-    Console.WriteLine("O segundo número: " + numero2 + " tem o maior valor");
+    Console.WriteLine("O valor de A é: " + numero1 + " o valor de C é: " + numero3+
+     " e o valor de B possui o maior valor que é: " + numero2);
 }
 else
 {
-    Console.WriteLine("O terceiro número: " + numero3 + " tem o maior valor");
+    Console.WriteLine("O valor de A é: " + numero1 + " o valor de B é: " + numero2+
+    " e o valor de C possui o maior valor que é: " + numero3);
 }
 
---
+++ 013 - O maior de 3 numeros inteiros/013 - O maior de 3 numeros inteiros/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/***************************************************************************
+13) Escreva um algoritmo que leia 3 números inteiros e mostre o maior deles.
+****************************************************************************/
+
+int numero1, numero2, numero3;
+
+Console.WriteLine("Informe o primeiro numero:");
+numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o segundo numero:");
+numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o terceiro numero:");
+numero3 = int.Parse(Console.ReadLine());
+
+if (numero1 > numero2 && numero1 > numero3)
+{
+    Console.WriteLine("O primeiro número: " + numero1 + " tem o maior valor");
+}
+else if (numero2 > numero1 && numero2 > numero3)
+{
+    Console.WriteLine("O segundo número: " + numero2 + " tem o maior valor");
+}
+else
+{
+    Console.WriteLine("O terceiro número: " + numero3 + " tem o maior valor");
+}
+
