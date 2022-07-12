a7ce4e5d2a7af9d1ace8ac0136523e0d691bdf4c
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-07-12 14:51:05 -0300

    Exercicios de Algoritmo

--- 016 - Cardápio de uma lancheria/016 - Cardápio de uma lancheria/Program.cs
+++ 016 - Cardápio de uma lancheria/016 - Cardápio de uma lancheria/Program.cs
@@ -8,7 +8,7 @@ Especificação	    Código	   Preço
 Cachorro quente	     100	   1,20
 Bauru simples	     101	   1,30
 Bauru com ovo	     102	   1,50
-Hambúrger	       103	       1,20
+Hambúrger	     103	      1,20
 Cheeseburguer	     104	   1,30
 Refrigerante	     105	   1,00
 
