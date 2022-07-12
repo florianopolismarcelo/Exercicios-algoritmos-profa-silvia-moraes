e376ccc384c0f26671fbeb09e2aa7486214deb90
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-07-12 14:48:51 -0300

    Exercicios de Algoritmo

---
+++ 016 - Cardápio de uma lancheria/016 - Cardápio de uma lancheria/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*********************************************************************************
+16. O cardápio de uma lancheria é o seguinte:
+
+Especificação	    Código	   Preço
+
+Cachorro quente	     100	   1,20
+Bauru simples	     101	   1,30
+Bauru com ovo	     102	   1,50
+Hambúrger	         103	   1,20
+Cheeseburguer	     104	   1,30
+Refrigerante	     105	   1,00
+
+Escrever um algoritmo que leia o código do item pedido, a quantidade e calcule 
+o valor a ser pago por aquele lanche. Considere que a cada execução somente será 
+calculado um item.
+********************************************************************************/
+
