02c761c555494c20b27820c349465d9e64a562f1
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-07-12 14:08:50 -0300

    Exercicios de Algoritmo

---
+++ 015 - Par ou impar e + ou -/015 - Par ou impar e + ou -/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/***************************************************************************
+15) Faça um algoritmo que leia um nº inteiro e mostre uma mensagem indicando 
+se este número é par ou ímpar, e se é positivo ou negativo.
+****************************************************************************/
+
+int numero;
+
+Console.WriteLine("Didite um número inteiro:");
+numero = int.Parse(Console.ReadLine());
+
+if (numero % 2 == 0 && numero > 0)
+{
+    Console.WriteLine("Número par e possitivo");
+}
+else if (numero % 2 == 1 && numero > 0)
+{
+    Console.WriteLine("Número impar e possitivo");
+}
+else if (numero % 2 == 0 && numero < 0)
+{
+    Console.WriteLine("Número par e negativo");
+}
+else
+{
+    Console.WriteLine("Número impar e negativo");
+}
