d6143ea69713961993abfec2a6c5a6274322cc6c
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-07-19 20:09:11 -0300

    Exercicios de Algoritmo

--- 024 - Duração do jogo em horas/024 - Duração do jogo em horas/Program.cs
+++ 024 - Duração do jogo em horas/024 - Duração do jogo em horas/Program.cs
@@ -6,4 +6,32 @@ horas inteiras) e calcula a duração do jogo em horas, sabendo-se que o tempo m
 horas e que o jogo pode iniciar em um dia e terminar no dia seguinte.
 **************************************************************************************************************/
 
+int horaInicio, horaFim, duracao = 0;
 
+Console.WriteLine("+------------------------+");
+Console.WriteLine("+--    DURAÇÃO JOGO    --+");
+Console.WriteLine("+                        +");
+Console.WriteLine("+------------------------+");
+
+Console.Write(" Digite a hora de inicio do jogo: ");
+horaInicio = int.Parse(Console.ReadLine());
+
+Console.Write("Digite a hora do fim do jogo: ");
+horaFim = int.Parse(Console.ReadLine());
+
+
+if (horaInicio > horaFim)
+
+{
+    duracao = (24 - horaInicio) + horaFim;
+    Console.WriteLine("----------------------");
+    Console.WriteLine("Duração do jogo é de: "+duracao+ "hs");
+    Console.WriteLine("----------------------");
+}
+else
+{
+    duracao = (horaFim - horaInicio);
+    Console.WriteLine("----------------------");
+    Console.WriteLine("Duração do jogo é de: " + duracao + "hs");
+    Console.WriteLine("----------------------");
+}
