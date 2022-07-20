72bff3d4c4aaec86a95ac9fdc7485a531264301f
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-07-20 19:14:11 -0300

    Exercicios de Algoritmo

--- 025 - Conjunto de 4 valores/025 - Conjunto de 4 valores/Program.cs
+++ 025 - Conjunto de 4 valores/025 - Conjunto de 4 valores/Program.cs
@@ -8,3 +8,32 @@ a) Se i=1 escrever os três valores a, b, c em ordem crescente.
 b) Se i=2 escrever os três valores a, b, c em ordem decrescente.
 c) Se i=3 escrever os três valores a, b, c de forma que o maior entre a, b, c fique dentre os dois.
 **************************************************************************************************************/
+
+float a, b, c;
+
+Console.WriteLine("+-------------------------------------------------+");
+Console.WriteLine("Digite valor de [a] qualquer valor de número real: ");
+Console.WriteLine("+-------------------------------------------------+");
+a = float.Parse(Console.ReadLine());
+
+Console.WriteLine("+-------------------------------------------------+");
+Console.WriteLine("Digite valor de [a] qualquer valor de número real: ");
+Console.WriteLine("+-------------------------------------------------+");
+a = float.Parse(Console.ReadLine());
+
+Console.WriteLine("+-------------------------------------------------+");
+Console.WriteLine("Digite valor de [a] qualquer valor de número real: ");
+Console.WriteLine("+-------------------------------------------------+");
+a = float.Parse(Console.ReadLine());
+
+Console.WriteLine("+-------------------------------------------------------------------+");
+Console.WriteLine("+   Digite [1] para ver o valor de a, b, c em ordem crescente.      +");
+Console.WriteLine("+                                                                   +");
+Console.WriteLine("+   Digite [2] para ver o valor de a, b, c em ordem decrescente.    +");
+Console.WriteLine("+                                                                   +");
+Console.WriteLine("+   Digite [3] para que o maior entre a, b, c fique dentre os dois. +");
+Console.WriteLine("+-------------------------------------------------------------------+");
+int i = int.Parse(Console.ReadLine());
+
+
+
