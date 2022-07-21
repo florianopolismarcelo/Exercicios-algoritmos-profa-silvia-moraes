17bd22ba4c8ceb28d8c27c93eb4c34ff237c538b
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-07-21 07:00:40 -0300

    Exercicios de Algoritmo

--- 026 - Menor número de notas (dinheiro)/026 - Menor número de notas (dinheiro)/Program.cs
+++ 026 - Menor número de notas (dinheiro)/026 - Menor número de notas (dinheiro)/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 /******************************************************************************************************
-26. Escrever um algoritmo que lê um valor em reais e calcula qual o menor número possível de notas de 
+26) Escrever um algoritmo que lê um valor em reais e calcula qual o menor número possível de notas de 
 100, 50, 10, 5 e 1 em que o valor lido pode ser decomposto. Escrever o valor lido e a relação de notas 
 necessárias.
 *******************************************************************************************************/
--- 026 - Menor número de notas (dinheiro)/026 - Menor número de notas (dinheiro)/obj/Debug/net6.0/026 - Menor número de notas (dinheiro).AssemblyInfo.cs
+++ 026 - Menor número de notas (dinheiro)/026 - Menor número de notas (dinheiro)/obj/Debug/net6.0/026 - Menor número de notas (dinheiro).AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("026 - Menor número de notas")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("026 - Menor número de notas (dinheiro)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("026 - Menor número de notas")]
-[assembly: System.Reflection.AssemblyTitleAttribute("026 - Menor número de notas")]
+[assembly: System.Reflection.AssemblyProductAttribute("026 - Menor número de notas (dinheiro)")]
+[assembly: System.Reflection.AssemblyTitleAttribute("026 - Menor número de notas (dinheiro)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
